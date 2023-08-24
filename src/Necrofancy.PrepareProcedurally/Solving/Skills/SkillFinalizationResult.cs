f60780d4e7acc758ea48a7af97172046ce390762
Necrofancy <NecrofancyEnterprises@gmail.com>
2023-08-23 20:37:38 -0700

    Adjust for Biotech genes

--- src/Necrofancy.PrepareProcedurally/Solving/Skills/SkillFinalizationResult.cs
+++ src/Necrofancy.PrepareProcedurally/Solving/Skills/SkillFinalizationResult.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using RimWorld;
 using Verse;
 
@@ -17,11 +18,37 @@ namespace Necrofancy.PrepareProcedurally.Solving.Skills
 
         public void ApplyTo(Pawn pawn)
         {
+            var passionsModdedByGenes = new Dictionary<SkillDef, PassionMod.PassionModType>();
+            foreach (var gene in pawn.genes.GenesListForReading.Where(x => x.Active && x.def.passionMod != null))
+            {
+                passionsModdedByGenes[gene.def.passionMod.skill] = gene.def.passionMod.modType;
+            }
             foreach (var skillRecord in pawn.skills.skills)
             {
                 var passionAndLevel = FinalRanges[skillRecord.def];
                 skillRecord.levelInt = Rand.RangeInclusive(passionAndLevel.Min, passionAndLevel.Max);
-                skillRecord.passion = passionAndLevel.Passion;
+                if (passionsModdedByGenes.TryGetValue(skillRecord.def, out var mod))
+                {
+                    switch (mod)
+                    {
+                        case PassionMod.PassionModType.AddOneLevel:
+                            skillRecord.passion = passionAndLevel.Passion == Passion.Major
+                                ? Passion.Major
+                                : passionAndLevel.Passion + 1;
+                            break;
+                        case PassionMod.PassionModType.DropAll:
+                            skillRecord.passion = Passion.None;
+                            break;
+                        default:
+                            skillRecord.passion = passionAndLevel.Passion;
+                            break;
+                    }
+                }
+                else
+                {
+                    skillRecord.passion = passionAndLevel.Passion;
+                }
+                
             }
         }
     }
