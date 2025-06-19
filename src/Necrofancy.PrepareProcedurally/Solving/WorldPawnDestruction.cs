8d2c1f44fe64e0b03fbe554de85607cae8c4ef49
Necrofancy <31827421+Necrofancy@users.noreply.github.com>
2025-06-19 00:31:49 -0700

    1.1.3.0 - cleanup world pawns
    World pawns are generated as hidden relationships, so we clean those up before randomization.
    
    (cherry picked from commit cb810cf816b7bda2df01fa4420f57d8a15a4bcca)

---
+++ src/Necrofancy.PrepareProcedurally/Solving/WorldPawnDestruction.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld.Planet;
+using Verse;
+
+namespace Necrofancy.PrepareProcedurally.Solving;
+
+/// <summary>
+/// Pawn generation will try to generate invisible world pawns in the background.
+/// This is a problem for mass-generating pawns, as this doesn't get cleaned up.
+/// </summary>
+public static class WorldPawnDestruction
+{
+    /// <summary>
+    /// Clean up the world pawns related to this pawn before randomization.
+    /// </summary>
+    public static void DestroyRelationships(Pawn pawn)
+    {
+        foreach (var related in pawn.relations.RelatedPawns)
+        {
+            if (related.IsWorldPawn())
+            {
+                Find.WorldPawns.RemoveAndDiscardPawnViaGC(related);
+            }
+        }
+    }
+}
